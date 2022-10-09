5ea459cde91ea1ec28aaf241e529e72abbcdbdad
Артём <av1965k@yandex.ru>
2022-10-09 07:09:44 +0400

    Вторая попытка отправить. Привёл код в соответствие с PEP8

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 # Из модуля models импортируем модель Post
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author', 'group') 
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
     list_editable = ('group',)
     # Добавляем интерфейс для поиска по тексту постов
-    search_fields = ('text',) 
+    search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строка
     empty_value_display = '-пусто-'
 
-admin.site.register(Post, PostAdmin) 
-admin.site.register(Group) 
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-# Create your views here.
+
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {'posts': posts}
     return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)

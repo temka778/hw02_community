46f788221a3cf03cd2f20e7886a3d533e76e2f92
Артём <av1965k@yandex.ru>
2022-10-11 04:07:16 +0530

    Устранил 7 предписаний ревьюера

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
+COUNT_OF_POSTS = 10
+
+
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related('group')[:COUNT_OF_POSTS]
     context = {'posts': posts}
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    tab_name = f'Записи сообщества {slug}'
+    posts = group.posts.select_related('group')[:COUNT_OF_POSTS]
     context = {
         'group': group,
-        'posts': posts,
-        'title': tab_name
+        'posts': posts
     }
     return render(request, 'posts/group_list.html', context)
